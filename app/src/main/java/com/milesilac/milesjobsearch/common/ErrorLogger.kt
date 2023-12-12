96b7ed53e35f4a3af679bd84a70bcbdd946a0be9
MilesIlac <smile.caliprf97@gmail.com>
2023-12-12 21:24:36 +0800

    adjusted ErrorLogger code

--- app/src/main/java/com/milesilac/milesjobsearch/common/ErrorLogger.kt
+++ app/src/main/java/com/milesilac/milesjobsearch/common/ErrorLogger.kt
@@ -2,7 +2,13 @@ package com.milesilac.milesjobsearch.common
 
 import android.util.Log
 
-fun logError(error: Exception, source: String = "Unknown Source") {
-    Log.e(source, error.localizedMessage ?: "Unknown error occurred")
+fun logError(source: String = "UnknownSource", error: Exception) {
     error.printStackTrace()
+    Log.e("${source}_Error", error.localizedMessage ?: "Unknown error occurred")
+
+}
+
+fun logError(source: String = "UnknownSource", error: Throwable) {
+    error.printStackTrace()
+    Log.e("${source}_Error", Log.getStackTraceString(error))
 }
