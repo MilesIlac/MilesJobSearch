31479fd7078e7cb89c70231a71db2c13a7122850
MilesIlac <smile.caliprf97@gmail.com>
2023-12-13 21:55:50 +0800

    added logError for String errors

--- app/src/main/java/com/milesilac/milesjobsearch/common/ErrorLogger.kt
+++ app/src/main/java/com/milesilac/milesjobsearch/common/ErrorLogger.kt
@@ -5,10 +5,13 @@ import android.util.Log
 fun logError(source: String = "UnknownSource", error: Exception) {
     error.printStackTrace()
     Log.e("${source}_Error", error.localizedMessage ?: "Unknown error occurred")
-
 }
 
 fun logError(source: String = "UnknownSource", error: Throwable) {
     error.printStackTrace()
     Log.e("${source}_Error", Log.getStackTraceString(error))
+}
+
+fun logError(source: String = "UnknownSource", error: String) {
+    Log.e("${source}_Error", error)
 }
