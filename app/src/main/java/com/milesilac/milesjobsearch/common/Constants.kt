1982ec8664132307e7932861da590d94d4e6a1c3
MilesIlac <smile.caliprf97@gmail.com>
2023-12-14 23:09:05 +0800

    Added test constans for api call

--- app/src/main/java/com/milesilac/milesjobsearch/common/Constants.kt
+++ app/src/main/java/com/milesilac/milesjobsearch/common/Constants.kt
@@ -5,6 +5,9 @@ object Constants {
     //THE MUSE API
     const val THE_MUSE_API_BASE_URL = "https://www.themuse.com/api/public/"
 
+    const val TEST_CATEGORY = "Software Engineering"
+    const val TEST_LOCATION = "Flexible / Remote"
+
     const val THE_MUSE_API_RATE_REMAINING = "x-ratelimit-remaining"
     const val THE_MUSE_API_RATE_LIMIT = "x-ratelimit-limit"
     const val THE_MUSE_API_RATE_RESET = "x-ratelimit-reset"
