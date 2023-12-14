afc280725334ab8addc32ba1005db69ce07e1530
MilesIlac <smile.caliprf97@gmail.com>
2023-12-14 23:11:45 +0800

    Added code to test out layout

--- app/src/main/java/com/milesilac/milesjobsearch/ui/feature/MainActivity.kt
+++ app/src/main/java/com/milesilac/milesjobsearch/ui/feature/MainActivity.kt
@@ -3,22 +3,48 @@ package com.milesilac.milesjobsearch.ui.feature
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.milesilac.milesjobsearch.ui.composables.JobListLayout
 import com.milesilac.milesjobsearch.domain.themuse.model.job.CompanyData
 import com.milesilac.milesjobsearch.domain.themuse.model.job.Job
 import com.milesilac.milesjobsearch.domain.themuse.model.job.JobCategory
 import com.milesilac.milesjobsearch.domain.themuse.model.job.JobLevel
 import com.milesilac.milesjobsearch.ui.theme.MilesJobSearchTheme
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lifecycleScope.launch {
+            viewModel.mainState.collect { state ->
+                when (state) {
+                    is MainState.GetJobsList -> {
+                        setContent {
+                            MilesJobSearchTheme {
+                                // A surface container using the 'background' color from the theme
+                                Surface(
+                                    modifier = Modifier.fillMaxSize(),
+                                    color = MaterialTheme.colorScheme.background
+                                ) {
+                                    JobListLayout(jobs = state.jobList.jobList)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         setContent {
             MilesJobSearchTheme {
                 // A surface container using the 'background' color from the theme
@@ -30,12 +56,13 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        viewModel.getJobsList()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun JobListLayoutPreview() {
     val jobs = listOf(
         Job(
             publicationDate = "January 1, 2025",
